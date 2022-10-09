6137bbf495734434563618ac51f0bddc5b0cf92d
🦈 <1319764245@qq.com>
2022-10-09 16:54:19 +0800

    1.复写表示：对父类的成员属性或成员方法进行重新定义 2.复写的方法：在子类中重新实现同名成员方法或成员属性即可 3.在子类中，如何调用父类成员 方式1：                           方式2： ·调用父类成员                      ·使用super()调用父类成员 使用成员变量：父类名.成员变量         使用成员变量：super().成员变量 使用成员方法：父类名.成员方法(self)   使用成员方法：siper().成员方法()
    ·注意：只可以在子类内部调用父类的同名成员，子类的实体类对象调用默认是调用子类复写的

---
+++ Grammar/第二阶段/10_继承_复写和使用父类成员.py
@@ -0,0 +1,27 @@
+class Phone:
+    IMEI = None   # 序列号
+    producer = "Z&F"
+
+    def call_by_5g(self):
+        print("使用5g网络进行通话")
+
+
+# 定义子类，复写父类成员
+class MyPhone(Phone):
+    producer = "ZAFU"
+
+    def call_by_5g(self):
+        print("开启CPU单核模式，确保通话的时候省电")
+        # 方式1：在子类中，调用父类成员
+        # print(f"父类的厂商是{Phone.producer}")
+        # Phone.call_by_5g(self)
+        #方式2：
+        print(f"父类的厂商是：{super().producer}")  # 调用父类的生产厂商
+        super().call_by_5g()  # 调用父类中的5g通话
+        print("关闭CPU单核模式，确保性能")
+
+
+phone = MyPhone()
+phone.call_by_5g()
+print(phone.producer)
+
