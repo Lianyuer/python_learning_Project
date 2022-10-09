2a8a1852e45bebf3c466db1a023b41ec30d36111
🦈 <1319764245@qq.com>
2022-10-09 23:34:55 +0800

    Revert "None"
    This reverts commit 6fdc6fc04660fa747012358b9806bc453c560c7f.

---
+++ Grammar/第一阶段/09_exception_module_pack/06_包.py
@@ -0,0 +1,26 @@
+# 创建一个包
+# 导入自定义的包中的模块，并使用
+import Grammar.第一阶段.my_package.my_module1
+import Grammar.第一阶段.my_package.my_module2
+
+Grammar.第一阶段.my_package.my_module1.info_print1()
+Grammar.第一阶段.my_package.my_module1.info_print2()
+# ----------------------------------------------
+from Grammar.第一阶段.my_package import my_module1, my_module2
+
+my_module1.info_print1()
+my_module1.info_print2()
+
+# ----------------------------------------------
+from Grammar.第一阶段.my_package.my_module1 import info_print1
+from Grammar.第一阶段.my_package.my_module2 import info_print2
+
+info_print1()
+info_print2()
+
+# 通过__all__变量，控制import *
+from Grammar.第一阶段.my_package import *
+my_module1.info_print1()
+my_module2.info_print2()
+
+
--- Grammar/第一阶段/09_exception_module_pack/07_综合案例.py
+++ Grammar/第一阶段/09_exception_module_pack/07_综合案例.py
@@ -1,9 +1,10 @@
 # 创建my_utils 包， 在包内创建：str_util.py 和 file_util.py 2个模块，并提供相应的函数
 
-from my_utils import file_util
+import Grammar.第一阶段.my_utils.str_util
+from Grammar.第一阶段.my_utils import file_util
 
-print(my_utils.str_util.str_reverse("浙江农林大学"))
-print(my_utils.str_util.substr("浙江农林大学", 1, 3))
+print(Grammar.第一阶段.my_utils.str_util.str_reverse("浙江农林大学"))
+print(Grammar.第一阶段.my_utils.str_util.substr("浙江农林大学", 1, 3))
 
 
 # file_util.print_file_info("E:/test.txt")
