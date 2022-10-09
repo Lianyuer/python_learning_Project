74ec4fde29e04a9aa6fd3d7ed6481ae34a9d0877
🦈 <1319764245@qq.com>
2022-10-09 10:48:35 +0800

    move

--- Grammar/第一阶段/09_exception_module_pack/07_综合案例.py
+++ Grammar/第一阶段/09_exception_module_pack/07_综合案例.py
@@ -1,10 +1,10 @@
 # 创建my_utils 包， 在包内创建：str_util.py 和 file_util.py 2个模块，并提供相应的函数
 
-import Grammar.my_utils.str_util
-from Grammar.my_utils import file_util
+import Grammar.第一阶段.my_utils.str_util
+from Grammar.第一阶段.my_utils import file_util
 
-print(Grammar.my_utils.str_util.str_reverse("浙江农林大学"))
-print(Grammar.my_utils.str_util.substr("浙江农林大学", 1, 3))
+print(Grammar.第一阶段.my_utils.str_util.str_reverse("浙江农林大学"))
+print(Grammar.第一阶段.my_utils.str_util.substr("浙江农林大学", 1, 3))
 
 
 # file_util.print_file_info("E:/test.txt")
