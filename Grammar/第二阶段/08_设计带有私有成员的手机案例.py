15e3df69376d24eb52d1cb2dc38c2c0a5077bed6
🦈 <1319764245@qq.com>
2022-10-09 16:03:44 +0800

    私有成员的实际意义： 在类中提供仅供内部使用的属性和方法，而不对外开发（类对象无法使用）

--- Grammar/第二阶段/08_设计带有私有成员的手机案例.py
+++ Grammar/第二阶段/08_设计带有私有成员的手机案例.py
@@ -9,16 +9,21 @@
     ·打印输出：正在通话中
 """
 
+# 设计一个类，用来描述手机
+
 
 class Phone:
-    __is_5g_enable = False
+    # 提供私有成员变量def __is_5g_enable：
+    __is_5g_enable = False  # 5g状态
+    # 提供私有成员方法def def __check_5g()：
 
     def __check_5g(self):
         if self.__is_5g_enable:
             print("5g开启")
         else:
             print("5g关闭，使用4g网络")
-
+    # 提供公开成员方法：call_by_5g()
+    
     def call_by_5g(self):
         self.__check_5g()
         print("正在通话中...")
